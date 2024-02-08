337b13b6ed02f08b74bcbc21a0a95c92c36006f4
DaniDev <dante.rodriguez.c@tecsup.edu.pe>
2024-02-08 13:16:22 -0500

    social network solved

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -43,8 +43,7 @@ function Home() {
         </p>
         <nav className="flex gap-3">
           <a
-            href="https://mail.google.com/mail/u/0/#inbox"
-            target="_blank"
+            href="mailto:dante.rodriguez.c@tecsup.edu.pe"
             rel="noopener noreferrer"
             role="link"
             className="inline-flex items-center justify-center gap-2 px-4 py-1 text-gray-800 transition bg-gray-100 border border-gray-300 rounded-full dark:bg-gray-800 dark:border-gray-600 dark:text-white focus-visible:ring-yellow-500/80 text-md hover:bg-gray-900 hover:border-gray-700 group max-w-fit hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black"
